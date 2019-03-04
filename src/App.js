af221102205f918df98819a57a04173dc4f0e1d7
antoanetad78 <antoanetad78@gmail.com>
2019-03-04 18:00:01 +0100

    dummy app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import MainView from './components/MainView';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+       
+       <div className='top-bar'>
+            <div className='level-title-score'>
+                <span>Level 1</span>
+                <span>Title: Learn Dog breeds</span>
+                <span>Score 5</span>
+            </div>
+            <div className='progress-bar'></div>
+        </div>
+
+        <div className='mainView'>
+                <div className='breedImage'>
+                    <img src='https://images.dog.ceo/breeds/elkhound-norwegian/n02091467_7265.jpg' alt=''/>
+                </div>
+                <span>Breed Name</span>
+                <span>Hint</span>
+                <form>
+                    <div className='radio'>
+                        <label>
+                            <input type='radio' value='Breed 1' checked={true} />
+                            Breed 1
+                        </label>
+                    </div>
+
+                    <div className='radio'>
+                        <label>
+                            <input type='radio' value='Breed 2' checked={true} />
+                            Breed 2
+                        </label>
+                    </div>  
+
+                    <div className='radio'>
+                        <label>
+                            <input type='radio' value='Breed 3' checked={true} />
+                            Breed 3
+                        </label>
+                    </div>                    
+                    
+                </form>
+            </div>
+       
       </div>
     );
   }
