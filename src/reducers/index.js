1e145cc796c576ef4a16930c837dbe17ac6a32ea
vdegraaf <vdegraaf.vincent@gmail.com>
2019-03-05 16:16:02 +0100

    minor bug fix

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,12 @@
 import { combineReducers } from 'redux'
 
-import reducer from './reducers'
-import reducer from './reducer'
+// import reducer from './reducers'
+// import reducer from './reducer'
 import levelUpReducer from './levelUpReducer'
 import answerBoolean from './answerBoolean'
 
 export default combineReducers({
-    reducer,
+    // reducer,
     levelUpReducer,
     answerBoolean
 })
