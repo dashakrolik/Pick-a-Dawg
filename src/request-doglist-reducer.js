e7afbb4d13433a3d2decd2e2b2d5f343a4e28696
vdegraaf <vdegraaf.vincent@gmail.com>
2019-03-05 10:56:37 +0100

    finished levelUp action and reducer. Needs to be checked with working app

--- src/request-doglist-reducer.js
+++ src/request-doglist-reducer.js
@@ -1,38 +1,100 @@
 import React, {Component} from 'react'
 import request from 'superagent'
 
-const dogList = 
+// const dogList = 
+// 		request
+// 			.get('https://dog.ceo/api/breeds/list/all')
+// 			.then(res => res.body.message)
+// 			.then(res => {
+// 				const randomNumber1 = Math.floor(Math.random()*Object.keys(res).length)
+// 				const randomNumber2 = Math.floor(Math.random()*Object.keys(res).length)
+// 				const randomNumber3 = Math.floor(Math.random()*Object.keys(res).length)
+// 				return ([
+// 					Object.keys(res)[randomNumber1],
+// 					Object.keys(res)[randomNumber2],
+// 					Object.keys(res)[randomNumber3]
+// 				])
+// 			})
+// 			.then(res => console.log(res, 'three dogs'))
+
+// REDUCER
+import { LEVEL_UP} from './actions/levelUp'
+
+export default (state = [], action = {}) => {
+
+	switch(action.type){
+			case 'LEVEL_UP': 
+					return [ ...state, { ...action.payload } ]             
+
+			default: 
+					return state
+	}
+}
+
+
+
+// this ACTION should be triggered as Initial state && everytime level goes up!			
+export function levelUpSetImages(images) {
+	return {
+		type: 'LEVEL_UP',
+		payload: images
+	}
+}
+
+export function levelUpGetImages() {
+	return function (dispatch) {
 		request
 			.get('https://dog.ceo/api/breeds/list/all')
 			.then(res => res.body.message)
-			.then(res => console.log(res, 'doglist length'))
-			// .then(Math.floor(Math.random()*dogList.length))
-			// .then(res => console.log(res, 'request'))
-			
+			.then(res => {
+				const randomNumber1 = Math.floor(Math.random()*Object.keys(res).length)
+				const randomNumber2 = Math.floor(Math.random()*Object.keys(res).length)
+				const randomNumber3 = Math.floor(Math.random()*Object.keys(res).length)
+				return ([
+					Object.keys(res)[randomNumber1],
+					Object.keys(res)[randomNumber2],
+					Object.keys(res)[randomNumber3]
+				])
+			})
+			.then(res => {
+				dispatch(levelUpSetImages(res))
+			})
 
-// const randomNumber = Math.floor(Math.random()*)
+	}
+}
+
+
+
+
+
+// example
+			export function setAlbums(albums) {
+				return {
+					type: SET_ALBUMS,
+					payload: albums
+				}
+			}
+			
+			export function getAlbums() {
+				return function (dispatch) {
+					request('https://jsonplaceholder.typicode.com/albums')
+						.then(response => {
+							dispatch(setAlbums(response.body))
+						})
+				}
+			}
+
 
 export default class FetchDogList extends Component {
 	
-	// componentDidMount() {  
-	// 	console.log('first check')
-
-
-	
 
 	render() {
-		console.log(dogList)
 		return (
-
-			
-		request
-		.get('https://dog.ceo/api/breeds/list/all')
-		.then(res => res.body.message)
-		.then(res => console.log(res, 'request'))
-	)}
-	}
-
-
-
-
-console.log('hee')
+			<div>{dogList}</div>
+		// request
+		// .get('https://dog.ceo/api/breeds/list/all')
+		// .then(res => res.body.message)
+		// .then(res => console.log(res, 'request'))
+	)
+}
+}
