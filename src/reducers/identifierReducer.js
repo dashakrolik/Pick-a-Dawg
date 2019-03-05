b0a188fe4da5e7072cc96aed1169abd4d6fe702d
DucMVu <vmd0210@gmail.com>
2019-03-05 16:18:49 +0100

    performance bar. streak missing. need to connect to other components

---
+++ src/reducers/identifierReducer.js
@@ -0,0 +1,10 @@
+const identifierReducer = (state = initialState, action = {}) => {
+    switch (action.type) {
+    default:
+      return state
+    }
+  }
+  export default reducer
+  
+  const initialState = []
+  
