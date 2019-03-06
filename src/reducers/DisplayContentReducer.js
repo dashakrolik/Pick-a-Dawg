4d45853147cf998de5ac9ef958c6e998a9e6c949
vdegraaf <vdegraaf.vincent@gmail.com>
2019-03-06 15:32:32 +0100

    possible to have more then 3 breeds in answer (so level up)

--- src/reducers/DisplayContentReducer.js
+++ src/reducers/DisplayContentReducer.js
@@ -1,13 +1,5 @@
 const reducer = (state = initialState, action = {}) => {
   switch (action.type) {
-    case 'UPDATE_IMAGE':
-    return action.payload
-
-    case 'CORRECT_ANSWER_ALERT':
-    return action.payload
-
-    case 'WRONG_ANSWER_ALERT':
-    return console.log('wrong')
 
     case 'UPDATE_ANSWER':
     return action.payload
