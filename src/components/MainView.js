d248eb4f086ab7bd5773591b449f1baff46b0512
antoanetad78 <antoanetad78@gmail.com>
2019-03-07 17:17:08 +0100

    add some buttons

--- src/components/MainView.js
+++ src/components/MainView.js
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import {connect} from 'react-redux'
 import ShowCorrectAnswer from '../actions/ShowCorrectAnswer'
+// import KeyboardEventHandler from 'react-keyboard-event-handler';
 
 class MainView extends React.Component {
 
@@ -10,6 +11,10 @@ class MainView extends React.Component {
     return `Hint: The name starts with: ${answerString[0]}`
   }
 
+
+// handleKeyPress = () => {
+
+// }
 
  handleClick = (event) => {
   // One single event triggers multiple dispatches on Redux store. In this case Tonia's answerBoolean and Duc's performance bar are both updated. REMEMBER: type's name should be identical!!!
@@ -49,11 +54,15 @@ class MainView extends React.Component {
           </div>
             <span>Breed Name</span>
             <span>{this.getHint(this.props.correctAnswer)}</span>
+            {/* <div>key detected: {this.props.eventKey}</div> */}
+            {/* <KeyboardEventHandler handleKeys={['up', 'down', 'enter/return']} onKeyEvent={(key, e) => console.log(`do something upon keydown event of ${key}`)}> */}
             <ul>
-              <li value={this.props.answer1} onClick={this.handleClick}>{this.props.answer1}</li>
-              <li value={this.props.answer2} onClick={this.handleClick}>{this.props.answer2}</li>
-              <li value={this.props.answer3} onClick={this.handleClick}>{this.props.answer3}</li>
-            </ul>
+              <li value={this.props.answer1} onClick={this.handleClick}><button autoFocus={true}>{this.props.answer1}</button></li>
+              <li value={this.props.answer2} onClick={this.handleClick}><button>{this.props.answer2}</button></li>
+              <li value={this.props.answer3} onClick={this.handleClick}><button>{this.props.answer3}</button></li>
+              </ul>
+            {/* </KeyboardEventHandler> */}
+            
             <ShowCorrectAnswer correctAnswer={this.props.correctAnswer} answerBoolean={this.props.answerBoolean}/>
             </div>
         )
