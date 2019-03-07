5b20ea704af94a57af646ac4fc8257f352b02507
DASHA <dariaepaneshnikova@icloud.com>
2019-03-07 15:29:50 +0100

    fixed displaying correct answer bug

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -6,7 +6,7 @@ import performanceBar from './performanceBarReducer'
 import levelUpReducer from './levelUpReducer'
 import answerBoolean from './answerBoolean'
 
-import ShowCorrectAnswerReducer from './ShowCorrectAnswerReducer'
+import showCorrectAnswer from './ShowCorrectAnswerReducer'
 import shownBreeds from './shownBreeds'
 
 export default combineReducers({
@@ -16,7 +16,7 @@ export default combineReducers({
     DisplayContentReducer,
     performanceBar,
 
-    ShowCorrectAnswerReducer,
+    showCorrectAnswer,
     shownBreeds
 
 })
